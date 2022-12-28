d418e8dd723ff16e23b93902acb09dbc0572052e
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-12-28 13:19:21 +0100

    added solution to the Coding Challenge #4. Worked with arrays, used for loop and reduce() method

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -183,34 +183,80 @@
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 // tall.
 
-const mark = {
-  firstName: "Mark",
-  lastName: "Miller",
-  mass: 78,
-  height: 178,
-  calcBMI: function () {
-    this.bmi = this.mass / Math.pow(this.height / 100, 2)
-    return this.bmi
-  },
+// const mark = {
+//   firstName: "Mark",
+//   lastName: "Miller",
+//   mass: 78,
+//   height: 178,
+//   calcBMI: function () {
+//     this.bmi = this.mass / Math.pow(this.height / 100, 2)
+//     return this.bmi
+//   },
+// }
+
+// const john = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   mass: 50,
+//   height: 192,
+//   calcBMI: function () {
+//     this.bmi = this.mass / Math.pow(this.height / 100, 2)
+//     return this.bmi
+//   },
+// }
+
+// mark.calcBMI()
+// john.calcBMI()
+// console.log(
+//   `${mark.bmi > john.bmi ? mark.firstName : john.firstName}'s BMI (${
+//     mark.bmi > john.bmi ? mark.bmi : john.bmi
+//   }) is higher than ${
+//     mark.bmi < john.bmi ? mark.firstName : john.firstName
+//   }'s (${mark.bmi < john.bmi ? mark.bmi : john.bmi})!`
+// )
+
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays 😉
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = []
+const totals = []
+
+for (let i = 0; i < bills.length; i++) {
+  let tip = bills[i] >= 50 && bills[i] <= 300 ? 0.15 * bills[i] : 0.2 * bills[i]
+  let total = bills[i] + tip
+  tips.push(Math.floor(tip))
+  totals.push(Math.floor(total))
 }
 
-const john = {
-  firstName: "John",
-  lastName: "Smith",
-  mass: 50,
-  height: 192,
-  calcBMI: function () {
-    this.bmi = this.mass / Math.pow(this.height / 100, 2)
-    return this.bmi
-  },
+console.log(bills)
+console.log(tips)
+console.log(totals)
+
+function calcAverage(arr) {
+  return arr.reduce((acc, current) => acc + current) / arr.length
 }
 
-mark.calcBMI()
-john.calcBMI()
-console.log(
-  `${mark.bmi > john.bmi ? mark.firstName : john.firstName}'s BMI (${
-    mark.bmi > john.bmi ? mark.bmi : john.bmi
-  }) is higher than ${
-    mark.bmi < john.bmi ? mark.firstName : john.firstName
-  }'s (${mark.bmi < john.bmi ? mark.bmi : john.bmi})!`
-)
+console.log(`Total average is ${calcAverage(totals)}`)
