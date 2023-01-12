ae9dbe191e628d23de5355da0417bac48766aa37
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-01-12 14:02:21 +0100

    added eventlistener to remove hidden class on modal and overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,5 +1,12 @@
 'use strict';
 const showModalBtn = document.querySelectorAll('.show-modal');
-const modal = document.querySelectorAll('.modal');
-const closeModalBtn = document.querySelectorAll('.close-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+
+showModalBtn.forEach(btn => {
+  btn.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
