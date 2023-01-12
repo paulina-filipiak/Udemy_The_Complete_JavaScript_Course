6ba9c23e55a89cd068e3079ff6171e2bb17530bc
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-01-12 14:48:13 +0100

    added keyboard event to close the tab

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -20,3 +20,13 @@ overlay.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 });
+
+// if(!modal.classList.contains("hidden")){
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape') {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
+// }
