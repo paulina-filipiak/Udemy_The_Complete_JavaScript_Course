4f1263d3b25f30d2bbebbc5cb5a7c97b3b2d8467
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-01-12 13:50:00 +0100

    added needed variables to JS

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+const showModalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelectorAll('.modal');
+const closeModalBtn = document.querySelectorAll('.close-modal');
+const overlay = document.querySelector('.overlay');
